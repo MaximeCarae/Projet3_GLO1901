ff8f56725ca4cd22fa160ce68893f5c3fb487cf3
Vincent Bergeron <vincent.bergeron.13@ulaval.ca>
2019-12-16 20:17:19 -0500

    aaaaaaa

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
 import api
 import time
-import quoridorX
+import quoridorx
 
 
 def analyser_commande():
@@ -50,7 +50,7 @@ class Partie:
     # Initialise une partie
     def jouer_partie(self):
         self.id, self.etat = api.débuter_partie(self.idul)
-        self.partie = quoridorX.QuoridorX(["auto", "serveur"])
+        self.partie = quoridorx.QuoridorX(["auto", "serveur"])
         self.jouer_tour()
     
     
@@ -60,57 +60,57 @@ class Partie:
         Returns:
             [None] -- []
         """
-        # Afficher l'état de jeu (graphique ou ASCII, selon self.fenetre)
-        self.afficher_partie()
+        while(True):
+            # Afficher l'état de jeu (graphique ou ASCII, selon self.fenetre)
+            arg_coup = self.afficher_partie()
 
-        # Fonction qui analyse les entrées de la ligne de commande
-        def parse_move():
-            string = input("Entrez votre coup ")
-            if string in ("q", "Q"):
-                return "q"
-            if string in ("a", "A"):
-                print("Utilisation: <Coup> <position x> <position y>\n" +
-                    "Types de coups:\nD: Déplacer le pion\nMV: Mur vertical\n" +
-                    "MH: Mur horizontal", sep=""
-                    )
-                return parse_move()
-            try:
-                string = string.split(" ")
-                return string[0], [int(string[1]), int(string[2])]
-            except:
-                print("Mauvaise entrée. Réessayez")
-                return parse_move()
+            # Fonction qui analyse les entrées de la ligne de commande
+            def parse_move():
+                string = input("Entrez votre coup ")
+                if string in ("q", "Q"):
+                    return "q"
+                if string in ("a", "A"):
+                    print("Utilisation: <Coup> <position x> <position y>\n" +
+                        "Types de coups:\nD: Déplacer le pion\nMV: Mur vertical\n" +
+                        "MH: Mur horizontal", sep=""
+                        )
+                    return parse_move()
+                try:
+                    string = string.split(" ")
+                    return string[0], [int(string[1]), int(string[2])]
+                except:
+                    print("Mauvaise entrée. Réessayez")
+                    return parse_move()
 
-        # Si mode pas automatique: Analyser les entrées de la ligne de commande
-        if not self.auto:
-            arg_coup = parse_move()
-            if arg_coup == "q":
-                return None
-        # Si mode automatique: caller jouer_coup pour le joueur 1
-        else:
-            coup =  self.partie.jouer_coup(1)
-            arg_coup = coup[0], coup[1]
-            print("\n\n\n\n", arg_coup, "\n\n\n\n")
+            # Si mode pas automatique: Analyser les entrées de la ligne de commande
+            if not self.auto:
+                arg_coup = parse_move()
+                if arg_coup == "q":
+                    return None
+            # Si mode automatique: caller jouer_coup pour le joueur 1
+            else:
+                coup =  self.partie.jouer_coup(1)
+                arg_coup = coup[0], coup[1]
+                print("\n\n\n\n", arg_coup, "\n\n\n\n")
                 
 
-        # Update l'état de jeu pour rester synchronisé avec l'état de jeu du serveur 
-        try:
-            self.partie.etat = self.api.jouer_coup(self.id, arg_coup[0], arg_coup[1])
-            # Le serveur renvoie les positions des personnages en listes et non tuples -> conversion en tuple
-            for i in range(2):
-                self.partie.etat["joueurs"][i]["pos"] = tuple(self.partie.etat["joueurs"][i]["pos"])
+            # Update l'état de jeu pour rester synchronisé avec l'état de jeu du serveur 
+            try:
+                self.partie.etat = self.api.jouer_coup(self.id, arg_coup[0], arg_coup[1])
+                # Le serveur renvoie les positions des personnages en listes et non tuples -> conversion en tuple
+                for i in range(2):
+                    self.partie.etat["joueurs"][i]["pos"] = tuple(self.partie.etat["joueurs"][i]["pos"])
 
 
-        # Exception si joueur est gagnant
-        except StopIteration as stop_iter:
-            print("Joueur gagnant: {}".format(stop_iter))
-            return None
+            # Exception si joueur est gagnant
+            except StopIteration as stop_iter:
+                print("Joueur gagnant: {}".format(stop_iter))
+                return None
 
-        # Exception si ça plante
-        except RuntimeError as run_time_error:
-            print("Erreur: {}".format(run_time_error))
+            # Exception si ça plante
+            except RuntimeError as run_time_error:
+                print("Erreur: {}".format(run_time_error))
 
-        self.jouer_tour()
 
 
     def afficher_partie(self):
@@ -127,5 +127,4 @@ class Partie:
 
 if __name__ == "__main__":
     commande = analyser_commande()
-    print(commande)
     Partie(commande.idul, commande.auto, commande.fenetre)
