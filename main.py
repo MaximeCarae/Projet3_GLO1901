8a77595ad93fd0a745194f096f39e08eb992a868
Vincent Bergeron <vincent.bergeron.13@ulaval.ca>
2019-12-16 22:51:48 -0500

    aaaa

--- main.py
+++ main.py
@@ -50,36 +50,36 @@ class Partie:
     def jouer_partie(self):
         self.id, self.etat = api.débuter_partie(self.idul)
         self.partie = QuoridorX(["auto", "serveur"])
-        self.jouer_tour()
+        self.jouer_tours()
     
     
-    def jouer_tour(self):
-        """[Effectue un tour de la partie de jeu]
+    def jouer_tours(self):
+        """[Effectue des tours de la partie de jeu]
 
         Returns:
             [None] -- []
         """
+        # Fonction qui analyse les entrées de la ligne de commande
+        def parse_move():
+            string = input("Entrez votre coup ")
+            if string in ("q", "Q"):
+                return "q"
+            if string in ("a", "A"):
+                print("Utilisation: <Coup> <position x> <position y>\n" +
+                    "Types de coups:\nD: Déplacer le pion\nMV: Mur vertical\n" +
+                    "MH: Mur horizontal", sep=""
+                    )
+                return parse_move()
+            try:
+                string = string.split(" ")
+                return string[0], [int(string[1]), int(string[2])]
+            except:
+                print("Mauvaise entrée. Réessayez")
+                return parse_move()
+
         while(True):
             # Afficher l'état de jeu (graphique ou ASCII, selon self.fenetre)
             arg_coup = self.afficher_partie()
-
-            # Fonction qui analyse les entrées de la ligne de commande
-            def parse_move():
-                string = input("Entrez votre coup ")
-                if string in ("q", "Q"):
-                    return "q"
-                if string in ("a", "A"):
-                    print("Utilisation: <Coup> <position x> <position y>\n" +
-                        "Types de coups:\nD: Déplacer le pion\nMV: Mur vertical\n" +
-                        "MH: Mur horizontal", sep=""
-                        )
-                    return parse_move()
-                try:
-                    string = string.split(" ")
-                    return string[0], [int(string[1]), int(string[2])]
-                except:
-                    print("Mauvaise entrée. Réessayez")
-                    return parse_move()
 
             # Si mode pas automatique: Analyser les entrées de la ligne de commande
             if not self.auto:
@@ -88,9 +88,7 @@ class Partie:
                     return None
             # Si mode automatique: caller jouer_coup pour le joueur 1
             else:
-                coup =  self.partie.jouer_coup(1)
-                arg_coup = coup[0], coup[1]
-                print("\n\n\n\n", arg_coup, "\n\n\n\n")
+                arg_coup =  self.partie.jouer_coup(1)
                 
 
             # Update l'état de jeu pour rester synchronisé avec l'état de jeu du serveur 
